2b20de834f8ada4f8a3ea824f536983b6d8c21ef
Lawrenze Valdez <valdezlawrenze@gmail.com>
2017-12-16 15:31:43 +0800

    Navbar

---
+++ admin/navbar.php
@@ -0,0 +1,68 @@
+<!----->
+        <nav class="navbar-default navbar-static-top" role="navigation">
+             <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+               <h1> <a class="navbar-brand" href="dashboard">Minimal</a></h1>         
+               </div>
+             <div class=" border-bottom">
+            <div class="full-left">
+
+            <div class="clearfix"> </div>
+           </div>
+     
+       
+            <!-- Brand and toggle get grouped for better mobile display -->
+         
+           <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="drop-men" >
+                <ul class=" nav_1">
+                    <li class="dropdown">
+                      <a href="#" class="dropdown-toggle dropdown-at" data-toggle="dropdown"><span class=" name-caret">Rackham<i class="caret"></i></span><img src="images/wo.jpg"></a>
+                      <ul class="dropdown-menu " role="menu">
+                        <li><a href="profile"><i class="fa fa-user"></i>Edit Profile</a></li>
+                        <li><a href="inbox"><i class="fa fa-envelope"></i>Inbox</a></li>
+                        <li><a href="calendar"><i class="fa fa-calendar"></i>Calender</a></li>
+                        <li><a href="inbox"><i class="fa fa-clipboard"></i>Tasks</a></li>
+                      </ul>
+                    </li>
+                   
+                </ul>
+             </div><!-- /.navbar-collapse -->
+            <div class="clearfix">
+       
+     </div>
+      
+            <div class="navbar-default sidebar" role="navigation">
+                <div class="sidebar-nav navbar-collapse">
+                <ul class="nav" id="side-menu">
+                
+                    <li>
+                        <a href="dashboard" class=" hvr-bounce-to-right"><i class="fa fa-dashboard nav_icon "></i><span class="nav-label">Dashboards</span> </a>
+                    </li>
+                     <li>
+                        <a href="#" class=" hvr-bounce-to-right"><i class="fa fa-desktop nav_icon"></i> <span class="nav-label">Clubs</span><span class="fa arrow"></span></a>
+                        <ul class="nav nav-second-level">
+                            <li><a href="math" class=" hvr-bounce-to-right"> <i class="fa fa-info-circle nav_icon"></i>Math</a></li>
+                            <li><a href="english" class=" hvr-bounce-to-right"><i class="fa fa-question-circle nav_icon"></i>English</a></li>
+                            <li><a href="science" class=" hvr-bounce-to-right"><i class="fa fa-file-o nav_icon"></i>Science</a></li>
+                       </ul>
+                    </li>
+                     <li><a href="officers" class=" hvr-bounce-to-right"><i class="fa fa-file-o nav_icon"></i>Club Officers</a></li>
+
+                   
+                    <!-- <li>
+                        <a href="#" class=" hvr-bounce-to-right"><i class="fa fa-cog nav_icon"></i> <span class="nav-label">Settings</span><span class="fa arrow"></span></a>
+                        <ul class="nav nav-second-level">
+                            <li><a href="signin" class=" hvr-bounce-to-right"><i class="fa fa-sign-in nav_icon"></i>Signin</a></li>
+                            <li><a href="signup" class=" hvr-bounce-to-right"><i class="fa fa-sign-in nav_icon"></i>Singup</a></li>
+                        </ul>
+                    </li> -->
+                </ul>
+            </div>
+            </div>
+        </nav>
